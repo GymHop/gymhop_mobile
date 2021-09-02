23f399da69f10607766a7869d26f6c6e361762ca
jerrysip <75126303+jerrysip@users.noreply.github.com>
2021-09-02 14:16:51 -0400

    drawer and header (#22)

--- src/screens/MapHomeScreen/components/Map1ScreenView.js
+++ src/screens/MapHomeScreen/components/Map1ScreenView.js
@@ -1,12 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react';
-import {Button, View, Text, ImageBackground, Image, Platform, StyleSheet} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  Button,
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  Platform,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity
+} from 'react-native';
 import {Map} from '../../../components/map';
 import {Res} from '../../../resources';
 import {Measurements} from '../../../utils';
 import styled from 'styled-components/native';
-import PremIcon from '../../../assets/icons/mapMarkerPremium.png';
-import StanIcon from '../../../assets/icons/mapMarkerStandard.png';
-import { Marker } from 'react-native-maps';
+import {DrawerActions} from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
+
+function DrawerButton({}) {
+  const navigation = useNavigation();
+  return (
+    <SafeAreaView>
+      <TouchableOpacity
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}>
+        <Image source={require('../../../assets/icons/menu_24px.png')} />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
 
 const Container = styled.KeyboardAvoidingView`
   flex: 1;
@@ -18,28 +39,62 @@ const Container = styled.KeyboardAvoidingView`
 `;
 
 export const Map1ScreenView = props => {
-  const [userRegion, setUserRegion] = useState({ 
+  const [userRegion, setUserRegion] = useState({
     latitude: 40.709318,
     longitude: -73.990686,
     latitudeDelta: 0.068,
     longitudeDelta: 0.033,
-  })
-  
-  return (
-    <Container>
-      <Map 
-      initialRegion={{"latitude": 40.709318,
-        "longitude": -73.990686,
-        "latitudeDelta": 0.068,
-        "longitudeDelta": 0.033,}}
-      latitude={userRegion.latitude}
-      longitude={userRegion.longitude}
-      latitudeDelta={userRegion.latitudeDelta}
-      longitudeDelta={userRegion.longitudeDelta}
-      userRegion={userRegion}
-      setUserRegion={setUserRegion}>
-      </Map>
-    </Container>
-  )
-}
+  });
 
+  return (
+    <View style={styles.wrap}>
+      <View style={styles.sample}>
+        <Text style={styles.ham}>
+          <DrawerButton />
+        </Text>
+        <View style={styles.logo}>
+          <Image style={styles.gymhoplogo} source={require('../../../assets/images/logos/GHLogo.png')} />
+        </View>
+      </View>
+
+      <Container style={styles.map}>
+        <Map
+          initialRegion={{
+            latitude: 40.709318,
+            longitude: -73.990686,
+            latitudeDelta: 0.068,
+            longitudeDelta: 0.033,
+          }}
+          latitude={userRegion.latitude}
+          longitude={userRegion.longitude}
+          latitudeDelta={userRegion.latitudeDelta}
+          longitudeDelta={userRegion.longitudeDelta}
+          userRegion={userRegion}
+          setUserRegion={setUserRegion}></Map>
+      </Container>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrap: {
+    height: '100%',
+  },
+  sample: {
+    backgroundColor: '#00C29E',
+    height: '12%',
+  },
+  logo: {
+    position: 'absolute',
+    top: 62,
+    left: 107,
+  },
+  ham: {
+    top: 67,
+    left: 30,
+  },
+  gymhoplogo:{
+    width: 180,
+    height: 24,
+  }
+});
