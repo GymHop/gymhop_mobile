c67970838047a0672ab38d1fbfd9946621f45c39
steffano <steffano.espinoza10@gmail.com>
2021-11-04 12:33:22 -0700

    Map list page and button

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,6 +1,6 @@
 import { PrimaryButton } from './PrimaryButton';
 import { SecondaryButton } from './SecondaryButton';
 import { PrimaryButtonTransparent } from './PrimaryButtonTransparent';
+import { MapListButton } from './MapListButton'
 
-export { PrimaryButton, PrimaryButtonTransparent, SecondaryButton };
-
+export { PrimaryButton, PrimaryButtonTransparent, SecondaryButton, MapListButton };
--- src/screens/MapHomeScreen/components/Map1ScreenView.js
+++ src/screens/MapHomeScreen/components/Map1ScreenView.js
@@ -2,13 +2,14 @@ import React, {useEffect, useState, useContext} from 'react';
 
 import {View, StyleSheet} from 'react-native';
 import {Map} from '../../../components/map';
+import { MapListButton } from '../../../components/buttons';
 import {Res} from '../../../resources';
 import {Measurements} from '../../../utils';
 import styled from 'styled-components/native';
 import {DrawerNavHeader} from '../../../components';
 import { useQuery } from 'react-query'
 import axios from 'axios'
-
+import { GymListTile } from '../../../components/individualGymComponents';
 
 
 const Container = styled.KeyboardAvoidingView`
@@ -20,6 +21,11 @@ const Container = styled.KeyboardAvoidingView`
   z-index: 3;
 `;
 
+const ContainerList = styled.ScrollView`
+  z-index:3;
+  display:flex;
+  `
+
 export const Map1ScreenView = props => {
   const [userRegion, setUserRegion] = useState({
     latitude: 40.709318,
@@ -28,13 +34,32 @@ export const Map1ScreenView = props => {
     longitudeDelta: 0.033,
   });
 
+  const [icon, setIcon] = useState('list')
+  const [markers, setMarkers] = useState([])
+
+  const { data, error, isLoading } = useQuery(
+    'gyms',
+    async () => {
+      const response = await axios.get('https://gymhop-api-staging.herokuapp.com/api/v1/gyms?latitude=40.7021&longitude=-73.9863196')
+      return response.data.data
+    }
+  )
+
+    useEffect(() => {
+      if (isLoading) return 'null';
+      if (error) return `Error! ${error.message}`;
+      if (data) setMarkers(data);
+    }, [data, isLoading, error])
+
 
   return (
     <View style={styles.wrap}>
       <DrawerNavHeader />
-
+      <MapListButton icon={icon} setIcon={setIcon} />
+      {icon == 'list' ?
       <Container style={styles.map}>
         <Map
+          markers={markers}
           initialRegion={{
             latitude: 40.709318,
             longitude: -73.990686,
@@ -48,6 +73,18 @@ export const Map1ScreenView = props => {
           userRegion={userRegion}
           setUserRegion={setUserRegion}></Map>
       </Container>
+      :<>
+      <ContainerList contentContainerStyle={styles.center} >
+        {markers.map( (gym, i) => {
+          gym.openClosed='open'
+          gym.rating='5.0'
+          gym.distance='6.7mi'
+          return <GymListTile key={gym.id} gym={gym} i={i} />
+        })}
+        <View style={{width:300, height:100}} />
+      </ContainerList>
+      </>
+      }
     </View>
   );
 };
@@ -77,4 +114,8 @@ const styles = StyleSheet.create({
     width: 180,
     height: 24,
   },
+  center :{
+    alignItems: 'center',
+    // justifyContent: 'center',
+  }
 });
