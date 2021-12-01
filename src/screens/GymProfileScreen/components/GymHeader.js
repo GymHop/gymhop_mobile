27f5ecfd875e6940b526abec78002a72e88689f3
Elyse Steingold <elyselindsays@gmail.com>
2021-12-01 01:05:58 -0500

    Gym profile

---
+++ src/screens/GymProfileScreen/components/GymHeader.js
@@ -0,0 +1,100 @@
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+import styled from 'styled-components/native';
+import {GymIcon} from '../../../components';
+import {SecondaryButton} from '../../../components/buttons';
+import {GymPhotos} from './GymPhotos';
+
+const GymProfileHeader = styled.View`
+  align-items: center;
+  background-color: #ffffff;
+`;
+export const LocationBlock = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 3px;
+`;
+export const SmallIcon = styled.Image`
+  width: 13px;
+  height: 20px;
+`;
+export const TextWithIcon = styled.Text`
+  color: #727272;
+  font-size: 16;
+  padding-left: 10;
+  padding-right: 10;
+`;
+const PremiumBadgeContainer = styled.View`
+  width: 100%;
+  align-items: flex-end;
+  margin-top: 8px;
+  margin-right: 26px;
+`;
+
+const styles = StyleSheet.create({
+  fontText: {
+    fontFamily: 'PlusJakartaSans-Regular',
+  },
+  profileTitle: {
+    fontFamily: 'PlusJakartaSans-Regular',
+    fontWeight: '700',
+    fontSize: 28,
+    padding: 5,
+    marginTop: 50,
+  },
+});
+
+export const GymHeader = ({gymData, distance, userData, tier}) => {
+  useEffect(() => {
+    console.log(userData);
+  }, []);
+
+  return (
+    <GymProfileHeader>
+
+      <View style={{position: 'absolute', bottom: 135}}>
+        <GymIcon
+          tier={gymData.tier}
+          logo_url={gymData.main_photo_url}
+          marker={false}
+        />
+      </View>
+
+      {tier === 'premium' ? (
+        <PremiumBadgeContainer>
+          <View style={{alignItems: 'center'}}>
+            <Image source={require('../../../assets/icons/premiumBadge.png')} />
+            <Text style={{color: '#F1CC52'}}>Premium</Text>
+          </View>
+        </PremiumBadgeContainer>
+      ) : null}
+
+      <Text style={styles.profileTitle}>{gymData.name}</Text>
+      <LocationBlock>
+        <SmallIcon source={require('../../../assets/icons/locationpin.png')} />
+        <TextWithIcon style={styles.fontText}>{gymData.address1}</TextWithIcon>
+      </LocationBlock>
+      <LocationBlock>
+        <SmallIcon
+          source={require('../../../assets/icons/distance.png')}
+          style={{height: 18, width: 11}}
+        />
+        <TextWithIcon style={styles.fontText}>{distance}</TextWithIcon>
+      </LocationBlock>
+      <View style={{alignItems: 'center', marginTop: 8, marginBottom: 16}}>
+        {/* {userData.current_tier === null && (
+          <SecondaryButton text={'See Memberships'} />
+        )}
+        {userData.current_tier === 'standard' && (
+          <SecondaryButton text={'Check In'} />
+        )}
+        {userData.current_tier === 'premium' && (
+          <SecondaryButton text={'Check In'} />
+        )} */}
+
+        <SecondaryButton text={'See Memberships'} />
+      </View>
+    </GymProfileHeader>
+  );
+};
